c030abbd32433828ce71f4d50e065fa54dfb30bf
jaaaw <csofcsc@gmail.com>
2025-03-06 00:27:44 +0200

    Add files via upload

---
+++ Verification successful!/sign-in.js
@@ -0,0 +1,10 @@
+document.getElementById('togglePassword').addEventListener('click', function () {
+    const passwordInput = document.getElementById('password');
+    const type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
+    passwordInput.setAttribute('type', type);
+  
+    // تبديل الأيقونة
+    this.classList.toggle('fa-eye');
+    this.classList.toggle('fa-eye-slash');
+  });
+  
